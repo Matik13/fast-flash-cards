63e04daa93d1c1fd021dbb71e8ad4db00269c123
Matik <126298922+Matik13@users.noreply.github.com>
2024-11-09 00:00:18 +0100

    Unit 7 + fixed bug in multiple choice mode

--- scripts.js
+++ scripts.js
@@ -1,68 +1,73 @@
-const words = [
-	{ english: 'apricot', polish: 'morela' },
-	{ english: 'blackcurrant', polish: 'czarna porzeczka' },
-	{ english: 'gooseberry', polish: 'agrest' },
-	{ english: 'tangerine', polish: 'mandarynka' },
-	{ english: 'aubergine', polish: 'bakłażan' },
-	{ english: 'chives', polish: 'szczypiorek' },
-	{ english: 'kale', polish: 'jarmuż' },
-	{ english: 'leek', polish: 'por' },
-	{ english: 'cod', polish: 'dorsz' },
-	{ english: 'poultry', polish: 'drób' },
-	{ english: 'prawn', polish: 'krewetka' },
-	{ english: 'crumble', polish: 'ciasto owocowe z kruszonką' },
-	{ english: 'packet of crisps', polish: 'paczkę czipsów' },
-	{ english: 'jug of milk', polish: 'dzbanek mleka' },
-	{ english: 'spoonful of sugar', polish: 'łyżeczka cukru' },
-	{ english: 'brunch', polish: 'późne śniadanie' },
-	{ english: 'grate', polish: 'zetrzeć na tarce' },
-	{ english: 'sprinkle', polish: 'posypać, pokropić' },
-	{ english: 'gulp', polish: 'połknąć' },
-	{ english: 'rinse', polish: 'opłukać' },
-	{ english: 'stew', polish: 'dusić na wolnym ogniu' },
-	{ english: 'kettle', polish: 'czajnik' },
-	{ english: 'crockery', polish: 'naczynia, porcelana stołowa' },
-	{ english: 'cutlery', polish: 'sztućce' },
-	{ english: 'sieve', polish: 'sitko' },
-	{ english: 'edible', polish: 'jadalny' },
-	{ english: 'inedible', polish: 'niejadalny' },
-	{ english: 'mild', polish: 'łagodny' },
-	{ english: 'sharp', polish: 'ostry, cierpki' },
-	{ english: 'bland', polish: 'mdły' },
-	{ english: 'lumpy', polish: 'grudkowaty' },
-	{ english: 'off-putting', polish: 'odpychający' },
-	{ english: 'revolting', polish: 'odrażający, okropny' },
-	{ english: 'scrumptious', polish: 'przepyszny' },
-	{ english: 'additives', polish: 'dodatki do żywności, np. barwniki' },
-	{ english: 'crash diet', polish: 'intensywna dieta odchudzająca' },
-	{ english: 'cut down on', polish: 'ograniczyć' },
-	{ english: 'put on weight', polish: 'przytyć' },
-	{ english: 'well-balanced diet', polish: 'zrównoważona dieta' },
-	{ english: 'daily intake', polish: 'dzienna dawka' },
-	{ english: 'high-fibre', polish: 'bogate w błonnik' },
-	{ english: 'nutritious', polish: 'pożywny' },
-	{ english: 'processed food', polish: 'żywność przetworzona' },
-	{ english: 'saturated fats', polish: 'tłuszcze nasycone' },
-	{ english: 'wholemeal', polish: 'razowy' },
-	{ english: 'bill', polish: 'rachunek' },
-	{ english: 'cuisine', polish: 'kuchnia, sposób gotowania' },
-	{ english: 'main course', polish: 'danie główne' },
-	{ english: 'review', polish: 'recenzja' },
-	{ english: 'self-service', polish: 'samoobsługa, samoobsługowy' },
-	{ english: 'takeaway', polish: '(danie) na wynos' },
-	{ english: 'delicacies', polish: 'przysmaki, delikatesy' },
-	{ english: 'side dish', polish: 'dodatek do dania głównego' },
-	{ english: 'binge eating disorder', polish: 'zespół napadowego objadania się' },
-	{ english: 'compulsive', polish: 'kompulsywny' },
-	{ english: 'fixated on sth', polish: 'mieć obsesję na punkcie czegoś' },
-	{ english: 'malnutrition', polish: 'niedożywienie' },
-	{ english: 'overly concerned', polish: 'nadmiernie zaniepokojony' },
-	{ english: 'restrictive eating patterns', polish: 'ograniczony schemat żywienia' },
-]
-
+const words = {
+	'vending machine': 'automat (z napojami lub przekąskami)',
+	'DIY store': 'sklep z artykułami do majsterkowania',
+	'duty-free': 'sklep wolnocłowy',
+	'go window-shopping': 'oglądać wystawy sklepowe',
+	laundrette: 'pralnia samoobsługowa',
+	'pop-up store': 'sklep działający w określonym sezonie',
+	'retail outlet': 'punkt sprzedaży detalicznej',
+	apparel: 'odzież',
+	footwear: 'obuwie',
+	'household chemicals': 'chemia gospodarcza',
+	stationery: 'artykuły biurowe',
+	'end of line': 'końcówka kolekcji',
+	'fair price': 'uczciwa cena',
+	'value for money': 'towar wart swojej ceny',
+	affordable: 'w zasięgu możliwości finansowych',
+	unaffordable: 'poza zasięgiem możliwości finansowych',
+	'cheap and cheerful': 'tani, ale dobrej jakości',
+	'for a song': 'tani',
+	'low key': 'skromny',
+	upmarket: 'z wyższej półki',
+	bargain: 'okazja cenowa',
+	branch: 'oddział, filia',
+	charge: 'pobierać (opłatę), liczyć sobie (za coś)',
+	convenient: 'wygodny, dogodny',
+	'keep a receipt': 'zachować paragon',
+	'put up the price': 'podwyższyć cenę',
+	trolley: 'wózek sklepowy',
+	aisle: 'alejka (w supermarkecie)',
+	'buy in bulk': 'kupować hurtowo',
+	'cost next to nothing': 'kosztować tyle, co nic',
+	'meet expectations': 'spełniać oczekiwania',
+	'pay in installments': 'kupić na raty',
+	'pay upfront': 'płacić z góry',
+	'pick up a bargain': 'znaleźć okazję',
+	'put sth aside': 'odłożyć coś (w sklepie)',
+	'shortage of': 'brak',
+	'loose change': 'drobne',
+	'flyer/leaflet': 'ulotka',
+	'celebrity endorsement': 'udział znanej osoby w reklamie',
+	'cold calling': 'akwizycja przez telefon',
+	tailor: 'krawiec/krawcowa',
+	caterer: 'osoba/firma zajmująca się kateringiem',
+	'private tutor': 'korepetytor/korepetytorka',
+	solicitor: 'adwokat, radca prawny',
+	undertaker: 'zakład/przedsiębiorca pogrzebowy',
+	refund: 'zwrot pieniędzy',
+	faded: 'spłowiały',
+	scratched: 'zarysowany',
+	shrunk: 'skurczony',
+	torn: 'rozdarty',
+	'bank loan': 'kredyt w banku',
+	'cash flow': 'przepływ gotówki',
+	currency: 'waluta',
+	'go in the red': 'być na minusie (jak Rokita)',
+	overdraft: 'debet',
+	'pay interest': 'płacić odsetki',
+	'save money for a rainy day': 'oszczędzać na czarną godzinę',
+	shareholder: 'akcjonariusz',
+	'stocks and bonds': 'akcje i obligacje',
+	withdraw: 'wypłacić (pieniądze z banku)',
+	'fraudulent claim': 'fałszywe roszczenie',
+	'make a claim': 'wystąpić o odszkodowanie',
+	policyholder: 'osoba ubezpieczona',
+	'cooling-off period': 'okres odstąpienia od umowy',
+	'make a claim against sb': 'wystąpić z roszczeniem przeciwko komuś',
+}
 let knownWords = []
 let currentMode = ''
-let totalWords = words.length
+let totalWords = Object.keys(words).length
 
 document.querySelectorAll('#total-words').forEach(el => (el.innerText = totalWords))
 
@@ -88,7 +93,7 @@ function startFlashcards() {
 }
 
 function loadNextFlashcard() {
-	const unknownWords = words.filter(word => !knownWords.includes(word.english))
+	const unknownWords = Object.keys(words).filter(word => !knownWords.includes(word))
 	if (unknownWords.length === 0) {
 		updateProgress()
 		return
@@ -96,17 +101,17 @@ function loadNextFlashcard() {
 	const randomWord = unknownWords[Math.floor(Math.random() * unknownWords.length)]
 	let isFlipped = false
 
-	document.getElementById('word').innerText = randomWord.polish
+	document.getElementById('word').innerText = words[randomWord]
 	document.getElementById('flip').onclick = () => {
 		if (isFlipped) {
-			document.getElementById('word').innerText = randomWord.polish
+			document.getElementById('word').innerText = words[randomWord]
 		} else {
-			document.getElementById('word').innerText = randomWord.english
+			document.getElementById('word').innerText = randomWord
 		}
 		isFlipped = !isFlipped
 	}
 	document.getElementById('know').onclick = () => {
-		knownWords.push(randomWord.english)
+		knownWords.push(randomWord)
 		loadNextFlashcard()
 		updateProgress()
 	}
@@ -121,30 +126,30 @@ function startMultipleChoice() {
 }
 
 function loadNextMultipleChoiceQuestion() {
-	const unknownWords = words.filter(word => !knownWords.includes(word.english))
+	const unknownWords = Object.keys(words).filter(word => !knownWords.includes(word))
 	if (unknownWords.length === 0) {
 		updateProgress()
 		return
 	}
 	const randomWord = unknownWords[Math.floor(Math.random() * unknownWords.length)]
 
-	document.getElementById('question').innerText = randomWord.polish
+	document.getElementById('question').innerText = words[randomWord]
 
 	const options = []
 	while (options.length < 3) {
-		const randomOption = words[Math.floor(Math.random() * words.length)].english
-		if (!options.includes(randomOption) && randomOption !== randomWord.english) {
+		const randomOption = Object.keys(words)[Math.floor(Math.random() * totalWords)]
+		if (!options.includes(randomOption) && randomOption !== randomWord) {
 			options.push(randomOption)
 		}
 	}
-	options.push(randomWord.english)
+	options.push(randomWord)
 	options.sort(() => Math.random() - 0.5)
 
 	document.getElementById('options').innerHTML = options
 		.map(
 			option => `
-      <button onclick="checkMultipleChoiceAnswer('${option}', '${randomWord.english}')">${option}</button>
-    `
+        <button onclick="checkMultipleChoiceAnswer('${option}', '${randomWord}')" class="answer-option">${option}</button>
+      `
 		)
 		.join('')
 }
@@ -155,11 +160,16 @@ function checkMultipleChoiceAnswer(selected, correct) {
 	document.getElementById('multiple-choice-result').innerText = result
 	document.getElementById('multiple-choice-result').className = resultClass
 
+	// Dezaktywacja przycisków odpowiedzi
+	document.querySelectorAll('.answer-option').forEach(button => (button.disabled = true))
+
 	if (selected === correct) {
 		knownWords.push(correct)
 		updateProgress()
 	}
-	setTimeout(loadNextMultipleChoiceQuestion, 10)
+
+	// Odczekaj chwilę i przejdź do następnego pytania
+	setTimeout(loadNextMultipleChoiceQuestion, 1000)
 }
 
 function startTypingTest() {
@@ -170,31 +180,30 @@ function startTypingTest() {
 }
 
 function loadNextTypingQuestion() {
-  document.getElementById('next-question').classList.add('hidden');
-  const unknownWords = words.filter(word => !knownWords.includes(word.english));
-  if (unknownWords.length === 0) {
-    updateProgress();
-    return;
-  }
-  const randomWord = unknownWords[Math.floor(Math.random() * unknownWords.length)];
+	document.getElementById('next-question').classList.add('hidden')
+	const unknownWords = Object.keys(words).filter(word => !knownWords.includes(word))
+	if (unknownWords.length === 0) {
+		updateProgress()
+		return
+	}
+	const randomWord = unknownWords[Math.floor(Math.random() * unknownWords.length)]
 
-  document.getElementById('typing-question').innerText = randomWord.polish;
-  document.getElementById('typing-answer').value = '';
-  document.getElementById('typing-result').innerText = '';
+	document.getElementById('typing-question').innerText = words[randomWord]
+	document.getElementById('typing-answer').value = ''
+	document.getElementById('typing-result').innerText = ''
 
-  // Nasłuchiwacz klawisza Enter
-  document.getElementById('typing-answer').addEventListener('keydown', function(event) {
-    if (event.key === 'Enter') {
-      checkTypingAnswer();
-    }
-  });
+	// Nasłuchiwacz klawisza Enter
+	document.getElementById('typing-answer').addEventListener('keydown', function (event) {
+		if (event.key === 'Enter') {
+			checkTypingAnswer()
+		}
+	})
 }
-
 
 function checkTypingAnswer() {
 	const question = document.getElementById('typing-question').innerText
 	const userAnswer = document.getElementById('typing-answer').value.trim()
-	const correctWord = words.find(word => word.polish === question).english
+	const correctWord = Object.keys(words).find(key => words[key] === question)
 
 	if (userAnswer.toLowerCase() === correctWord.toLowerCase()) {
 		document.getElementById('typing-result').innerText = 'Poprawnie!'
